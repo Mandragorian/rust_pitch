f0ebb83dcabc92b82da070c694a887944e9dd485
Konstantinos Andrikopoulos <mandragore@protonmail.com>
2021-10-15 22:07:43 +0200

    iterate_rust: How to actually do it
    Of course, sometimes, we might actually want to iterate over the list
    as long as there are more elements in the list. He have ways to do
    that.

--- why_rust/comparison/iterate_rust/src/main.rs
+++ why_rust/comparison/iterate_rust/src/main.rs
@@ -1,7 +1,10 @@
 fn main() {
     let mut list = vec![1,2,3,4];
 
-    for elem in &list {
-        list.push(*elem);
+    let mut i = 0;
+    while i < list.len() {
+        println!("{}", list[i]);
+        list.push(list[i]);
+        i += 1;
     }
 }
