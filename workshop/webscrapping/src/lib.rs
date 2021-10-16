eae213ce581359155a72d2d9b3c4e96fa5e7101d
Konstantinos Andrikopoulos <mandragore@protonmail.com>
2021-10-16 02:11:37 +0200

    This is how you return more information on errs

--- workshop/webscrapping/src/lib.rs
+++ workshop/webscrapping/src/lib.rs
@@ -2,16 +2,16 @@ use reqwest::blocking::get;
 
 struct Scrapper;
 
-fn fetch(url: &str) -> Result<String, &str> {
+fn fetch(url: &str) -> Result<String, String> {
     let result = get(url);
     let text_result = match result {
         Ok(response) => response.text(),
-        Err(_) => return Err("fetching website failed"),
+        Err(err) => return Err(format!("fetching website failed: {}", err)),
     };
 
     match text_result {
         Ok(body) => Ok(body),
-        Err(_) => return Err ("fetching website failed"),
+        Err(_) => return Err (String::from("response has no body")),
     }
 }
 
@@ -55,7 +55,7 @@ mod test {
     fn handle_errors_when_fetching() {
         let text = match fetch("https://koko.lala") {
             Ok(_) => panic!("should not fetch non-existent website"),
-            Err(msg) => assert_eq!(msg, "fetching website failed"),
+            Err(msg) => assert_eq!(msg, "fetching website failed: error sending request for url (https://koko.lala/): error trying to connect: dns error: failed to lookup address information: Name or service not known"),
         };
     }
 }
