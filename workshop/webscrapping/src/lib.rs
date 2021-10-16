2ce67d4da6a1d1a9e96ee7ed71f56f6bb6c80551
Konstantinos Andrikopoulos <mandragore@protonmail.com>
2021-10-16 20:13:39 +0200

    Now lets try to actually push

--- workshop/webscrapping/src/lib.rs
+++ workshop/webscrapping/src/lib.rs
@@ -6,14 +6,14 @@ use std::sync::Arc;
 const WORDS: [&str; 4] = ["hello", "This", "bye", "website"];
 
 pub fn run() {
-    let v: Arc<Vec<&str>> = Arc::new(Vec::new());
+    let v  = Arc::new(Vec::new());
 
     let handles: Vec<_> = WORDS.iter().map(|word| {
         let v_cloned = Arc::clone(&v);
         std::thread::spawn(move || {
             let scrapper = scrapper::Scrapper::new("https://motherfuckingwebsite.com");
             if scrapper.contains(word) {
-                println!("{:?}", v_cloned);
+                v_cloned.push(word);
             }
         })
     }).collect();
